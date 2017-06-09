5804f615eb7a982027e2545419f8c534a2644dc2
mengxiangxiang <mengxiangxiang@cdavatar.com>
2017-06-09 17:20:36 +0800

    用 markdown-it 替代 remark 做语法解析

--- index.js
+++ index.js
@@ -1,54 +1,59 @@
 var loaderUtils = require('loader-utils');
-var unified = require('unified');
-var markdown = require('remark-parse');
-var stringify = require('remark-stringify');
-var html = require('remark-html');
+var MarkdownIt = require('markdown-it');
+var hljs = require('highlight.js');
 var path = require('path');
 
 module.exports = function (source) {
     // this.cacheable();
 
-    var node = unified()
-        .use(markdown, { commonmark: true })
-        .parse(source);
+    var options = {
+        html: true,
+        highlight: function (code) {
+            return hljs.highlightAuto(code).value;
+        }
+    };
+    var md = MarkdownIt(options);
+
+    var tokens = md.parse(source);
     
     var importScript = [];
     var script = [];
-    var newNodeChildren = [];
-    node.children.forEach(function (child) {
-        if (child.type === 'code') {
-            if (child.lang === 'html') {
-                newNodeChildren.push({
-                    type: 'html',
-                    value: child.value
-                });
+    var newTokens = [];
+    tokens.forEach(function (token) {
+        if (token.type === 'fence' && token.tag === 'code') {
+            if (token.info.trim() === 'html') {
+                var htmlToken = md.parse('<div></div>')[0];
+                htmlToken.content = token.content;
+                newTokens.push(htmlToken);
+
+                token.attrSet(':skip', true);
             }
-            if (child.lang === 'js') {
-                child.value = child.value.replace(/import[\s\S]*?from[\s\S]*?\n/g, function ($0) {
+            if (token.info.trim() === 'js') {
+                token.content = token.content.replace(/import[\s\S]*?from[\s\S]*?\n/g, function ($0) {
                     if (importScript.indexOf($0.replace(/\s|\n/g, '')) < 0) {
                         importScript.push($0);
                     }
                     return '';
                 })
-                script.push(child.value);
+                script.push(token.content);
+                
+                token.attrSet(':skip', true);
             }
         }
-        newNodeChildren.push(child);
+        newTokens.push(token);
     });
-    node.children = newNodeChildren;
-
-    var processed = unified()
-        .use(stringify)
-        .stringify(node);
-    var result = unified()
-        .use(markdown, { commonmark: true })
-        .use(html)
-        .processSync(processed).toString()
+    var result = md.renderer.render(newTokens, md.options);
+    // ['{', '}'].forEach(function (l) {
+    //     var re = RegExp('\\' + l + '\\' + l, 'g');
+    //     var hex = '&#x' + l.charCodeAt(0).toString(16).toUpperCase() + ';';
+    //     result = result.replace(re, hex + hex);
+    // });
+    result = result.replace(/\n/g, '<br>');
     var component = [
         importScript.join(''),
         'export const name = \'component-demo-input\';' +
         'avalon.component(name, {' +
-        '    template: `' + result + '`' +
+        '    template: `<div>' + result + '</div>`' +
         '});' +
         script.join('\n')
     ];
@@ -67,6 +72,8 @@ var mdContent = heredoc(function () {
 
 基本用法
 
+<div :skip="true"></div>
+
 ``` html
 <div :controller="doc-input-basic">
     <xmp is="ms-form" :widget="{$form: @$form}">
@@ -82,19 +89,18 @@ var mdContent = heredoc(function () {
 import * as avalon from 'avalon2';
 import { createForm } from 'ane';
 
-avalon.define({
+const vm = avalon.define({
     $id: 'doc-input-basic',
     value: '123',
     json: '',
-    $form: createForm(),
-    onInit() {
-        this.$form.onFieldsChange = (fields, record) => {
-            this.json = JSON.stringify(record);
+    $form: createForm({
+        onFieldsChange(fields, record) {
+            vm.json = JSON.stringify(record);
         }
-    }
+    })
 });
 ```
     */
 });
 
-//module.exports(mdContent);
+// module.exports(mdContent);
