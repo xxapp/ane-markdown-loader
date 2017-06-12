ce14fab8fe3cb89b3cd8a5c5537502786d5f2e4d
mengxiangxiang <mengxiangxiang@cdavatar.com>
2017-06-12 14:17:02 +0800

    组件名中加入时间戳，避免重复

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-var loaderUtils = require('loader-utils');
 var MarkdownIt = require('markdown-it');
 var hljs = require('highlight.js');
 var path = require('path');
 
 module.exports = function (source) {
-    // this.cacheable();
-
+    this.cacheable();
+    
     var options = {
         html: true,
         highlight: function (code) {
@@ -43,64 +42,13 @@ module.exports = function (source) {
         newTokens.push(token);
     });
     var result = md.renderer.render(newTokens, md.options);
-    // ['{', '}'].forEach(function (l) {
-    //     var re = RegExp('\\' + l + '\\' + l, 'g');
-    //     var hex = '&#x' + l.charCodeAt(0).toString(16).toUpperCase() + ';';
-    //     result = result.replace(re, hex + hex);
-    // });
-    result = result.replace(/\n/g, '<br>');
     var component = [
         importScript.join(''),
-        'export const name = \'component-demo-input\';' +
+        'export const name = \'component-demo-' + Date.now() + '\';' +
         'avalon.component(name, {' +
         '    template: `<div>' + result + '</div>`' +
         '});' +
         script.join('\n')
     ];
-    console.log(component.join('\n'));
     return component.join('\n');
 }
-
-function heredoc(fn) {
-    return fn.toString().replace(/^[^\/]+\/\*!?\s?/, '').
-    replace(/\*\/[^\/]+$/, '').trim().replace(/>\s*</g, '><')
-}
-
-var mdContent = heredoc(function () {
-    /*
-# 输入组件
-
-基本用法
-
-<div :skip="true"></div>
-
-``` html
-<div :controller="doc-input-basic">
-    <xmp is="ms-form" :widget="{$form: @$form}">
-        <ms-form-item :widget="{label:'名字'}">
-            <ms-input :widget="{id:'test-value',col:'name',value:@value,$rules:{required:true,message:'请输入名字'}}"></ms-input>
-        </ms-form-item>
-    </xmp>
-    <pre>{{@json}}</pre>
-</div>
-```
-
-``` js
-import * as avalon from 'avalon2';
-import { createForm } from 'ane';
-
-const vm = avalon.define({
-    $id: 'doc-input-basic',
-    value: '123',
-    json: '',
-    $form: createForm({
-        onFieldsChange(fields, record) {
-            vm.json = JSON.stringify(record);
-        }
-    })
-});
-```
-    */
-});
-
-// module.exports(mdContent);
